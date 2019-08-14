18b99057d158fa28c7e655b5b718b1588ab9b6d9
Jetsurf <jetsurf1@gmail.com>
2019-08-14 14:33:05 -0400

    Dev (#38)
    * Fixed DM's throwing exception on timeout
    
    * Fixed DM's throwing exception on timeout take 2
    
    * Fix to storedm timeout handling/bug

--- nsohandler.py
+++ nsohandler.py
@@ -100,9 +100,9 @@ class nsoHandler():
 		try:	
 			resp = await self.client.wait_for('message', timeout=7100, check=check1)
 		except:
-			if resp == None:
-				print("TIMEOUT: Keeping " + theMem.name + " in DM's")
-				await theMem.send("Didn't get a message from you, I'll DM you again when gear with " + theSkill + " appears in the shop!")
+			print("TIMEOUT: Keeping " + theMem.name + " in DM's")
+			await theMem.send("Didn't get a message from you, I'll DM you again when gear with " + theSkill + " appears in the shop!")
+			return
 
 		if 'no' in resp.content.lower():
 			stmt = 'DELETE FROM storedms WHERE clientid = %s AND ability = %s'
