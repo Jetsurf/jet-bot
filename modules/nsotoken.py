6de32d12096b67570938769234b8f886b6e75305
Jetsurf <crmeade@mtu.edu>
2021-03-02 18:17:59 +0000

    NSO Ver 1.9.0 -> 1.10.1

--- modules/nsotoken.py
+++ modules/nsotoken.py
@@ -200,7 +200,7 @@ class Nsotoken():
 
 	def get_session_token(self, session_token_code, auth_code_verifier):
 		head = {
-			'User-Agent':      'OnlineLounge/1.9.0 NASDKAPI Android',
+			'User-Agent':      'OnlineLounge/1.10.1 NASDKAPI Android',
 			'Accept-Language': 'en-US',
 			'Accept':          'application/json',
 			'Content-Type':    'application/x-www-form-urlencoded',
@@ -255,7 +255,7 @@ class Nsotoken():
 			'Content-Length': '439',
 			'Accept': 'application/json',
 			'Connection': 'Keep-Alive',
-			'User-Agent': 'OnlineLounge/1.9.0 NASDKAPI Android'
+			'User-Agent': 'OnlineLounge/1.10.1 NASDKAPI Android'
 		}
 		body = {
 			'client_id': '71b963c1b7b6d119',
@@ -270,7 +270,7 @@ class Nsotoken():
 			return
 
 		head = {
-			'User-Agent': 'OnlineLounge/1.9.0 NASDKAPI Android',
+			'User-Agent': 'OnlineLounge/1.10.1 NASDKAPI Android',
 			'Accept-Language': 'en-US',
 			'Accept': 'application/json',
 			'Authorization': 'Bearer ' + id_response["access_token"],
@@ -288,9 +288,9 @@ class Nsotoken():
 		head = {
 			'Host': 'api-lp1.znc.srv.nintendo.net',
 			'Accept-Language': 'en-US',
-			'User-Agent': 'com.nintendo.znca/1.9.0 (Android/7.1.2)',
+			'User-Agent': 'com.nintendo.znca/1.10.1 (Android/7.1.2)',
 			'Accept': 'application/json',
-			'X-ProductVersion': '1.9.0',
+			'X-ProductVersion': '1.10.1',
 			'Content-Type': 'application/json; charset=utf-8',
 			'Connection': 'Keep-Alive',
 			'Authorization': 'Bearer',
@@ -334,9 +334,9 @@ class Nsotoken():
 
 		head = {
 			'Host': 'api-lp1.znc.srv.nintendo.net',
-			'User-Agent': 'com.nintendo.znca/1.9.0 (Android/7.1.2)',
+			'User-Agent': 'com.nintendo.znca/1.10.1 (Android/7.1.2)',
 			'Accept': 'application/json',
-			'X-ProductVersion': '1.9.0',
+			'X-ProductVersion': '1.10.1',
 			'Content-Type': 'application/json; charset=utf-8',
 			'Connection': 'Keep-Alive',
 			'Authorization': 'Bearer ' + splatoon_token["result"]["webApiServerCredential"]["accessToken"],
