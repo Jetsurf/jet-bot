3ca5da2de6db77afa699b531ec26bbda6ef8e52a
Jetsurf <crmeade@mtu.edu>
2021-04-29 23:45:12 +0000

    Move home server chunking to inside startup

--- bot.py
+++ bot.py
@@ -68,12 +68,13 @@ async def on_ready():
 	global client, soundsDir, mysqlHandler, serverUtils, serverVoices, splatInfo, helpfldr, hs
 	global nsoHandler, nsoTokens, head, url, dev, owners, commandParser, doneStartup, acHandler
 
-	#This is needed due to no prsence intent, prod bot needs to find the devs in its primary server
-	print("Chunking home server (" + str(hs) + ") to find owners")
-	await client.get_guild(int(hs)).chunk()
-
 	if not doneStartup:
 		print('Logged in as,', client.user.name, client.user.id)
+		
+		#This is needed due to no prsence intent, prod bot needs to find the devs in its primary server
+		print("Chunking home server (" + str(hs) + ") to find owners")
+		await client.get_guild(int(hs)).chunk()
+		
 		#Get owners from Discord team api
 		print("Loading owners...")
 		theapp = await client.application_info()
