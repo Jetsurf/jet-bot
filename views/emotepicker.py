1794f0dca6828750dc36724fb591e77860ced460
Jetsurf <crmeade@mtu.edu>
2021-10-31 22:50:06 +0000

    Condense

--- views/emotepicker.py
+++ views/emotepicker.py
@@ -1,5 +1,6 @@
+import sys, discord
 sys.path.append('../modules')
-import discord, mysqlhandler
+import mysqlhandler
 
 class DropDown(discord.ui.Select):
 	def __init__(self, emoteSelect):
@@ -13,7 +14,7 @@ class DropDown(discord.ui.Select):
 			if emote.is_usable():
 				options.append(discord.SelectOption(label=emote.name, description=emote.name, emoji=emote)) #f"<:{emote.name}:{str(emote.id)}>"))
 
-		super().__init__(placeholder=msg, min_values=1, max_values=1, options=options, custom_id=self.val)
+		super().__init__(placeholder=msg, options=options, custom_id=self.val)
 
 	async def callback(self, interaction: discord.Interaction):
 		self.choice = self.values[0]
@@ -22,15 +23,11 @@ class confirm(discord.ui.Button):
 	def __init__(self):
 		super().__init__(style=discord.ButtonStyle.green, label='Confirm')
 
-
 	async def callback(self, interaction: discord.Interaction):
-		choices = 0
 		for opt in self.view.opts:
-			if opt.choice != "":
-				choices+=1
-		if choices != len(self.view.picks):
-			await interaction.response.send_message("Please provide a choice for each option", ephemeral=True)
-			return
+			if opt.choice == "":
+				await interaction.response.send_message("Please provide a choice for each option", ephemeral=True)
+				return
 
 		string = ""
 		for opt in self.view.opts:
@@ -39,7 +36,7 @@ class confirm(discord.ui.Button):
 					break
 
 			string+=f'{opt.val} : <:{emote.name}:{emote.id}>\n'
-
+			#Do MYSQL Stuff here
 		await interaction.response.edit_message(content=f"Choices:\n{string}", view=None)
 		self.view.stop()
 
