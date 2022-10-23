29b05942ae0312bf310820c185dc6de073c1af7c
Andrew Wendt <andy@putergeek.com>
2022-10-23 13:31:35 -0700

    More translation groundwork.

--- modules/gameinfo/matchset.py
+++ modules/gameinfo/matchset.py
@@ -29,10 +29,11 @@ class MatchItem():
 
 
 class MatchResult():
-	def __init__(self, name, query, items):
-		self.name  = name
-		self.query = query
-		self.items = items
+	def __init__(self, name, query, items, language = "en-US"):
+		self.name     = name
+		self.query    = query
+		self.items    = items
+		self.language = language
 
 	def isValid(self):
 		return len(self.items) == 1
@@ -45,20 +46,19 @@ class MatchResult():
 	def errorMessage(self, listhelp = None):
 		l = len(self.items)
 		if l == 0:
-			msg = "I don't know of any " + self.name + " named '" + self.query + "'."
+			msg = "I don't know of any %s named '%s'." % (self.name, self.query)
 			if listhelp:
 				msg += " " + listhelp
 			return msg
 		elif l == 1:
 			return None
 		elif l == 2:
-			return "Did you mean " + self.items[0].format() + " or " + self.items[1].format() + "?"
+			return "Did you mean %s or %s?" % (self.items[0].format(self.language), self.items[1].format(self.language))
 		elif l < 8:
-			str = "Did you mean " + ", ".join(map(lambda item: item.format(), self.items[0:l - 1]))
-			str += ", or " + self.items[l - 1].format() + "?"
+			str = "Did you mean %s, or %s?" % (", ".join(map(lambda item: item.format(self.language), self.items[0:l - 1])), self.items[l - 1].format(self.language))
 			return str
 
-		msg = "What is '" + self.query + "'?"
+		msg = "What is '%s'?" % (self.query)
 		if listhelp:
 			msg += " " + listhelp
 		return msg
@@ -84,7 +84,7 @@ class MatchSet():
 
 	def matchItem(self, query, language = "en-US"):
 		if len(query) == 0:
-			return MatchResult(self.name, query, [])
+			return MatchResult(self.name, query, [], language)
 
 		query = query.lower()
 		items = []
@@ -92,14 +92,14 @@ class MatchSet():
 		# Stage 1: Look for exact match on a name or abbreviation
 		for item in self.items:
 			if item.hasAbbrev(query) or (item.name(language).lower() == query):
-				return MatchResult(self.name, query, [item])
+				return MatchResult(self.name, query, [item], language)
 
 		# Stage 2: Substring match of name
 		for item in self.items:
 			if item.name(language).lower().find(query) != -1:
 				items.append(item)
 
-		return MatchResult(self.name, query, items)
+		return MatchResult(self.name, query, items, language)
 
 	def getItemByName(self, name, language = "en-US"):
 		for i in self.items:
