85096a1a679cd3b3ec90dbdf7a342751e2d17ded
Andrew Wendt <andy@putergeek.com>
2022-10-31 18:30:47 -0700

    Move embed methods to S3EmbedBuilder.

--- modules/s3handler.py
+++ modules/s3handler.py
@@ -3,154 +3,15 @@ import mysqlhandler, nsotoken
 import json, sys, re, time, requests, random, hashlib, os, io
 import s3.storedm
 import s3.schedule
+
 from s3.imagebuilder import S3ImageBuilder
+from s3.embedbuilder import S3EmbedBuilder
 
 from io import BytesIO
 from os.path import exists
 import base64
 import datetime
 import dateutil.parser
-
-class S3Utils():
-	@classmethod
-	def createSplatfestEmbed(cls, splatfest):
-		now = datetime.datetime.now(datetime.timezone.utc)
-
-		starttime = dateutil.parser.isoparse(splatfest['startTime'])
-		endtime   = dateutil.parser.isoparse(splatfest['endTime'])
-
-		state = splatfest.get('state')
-
-		description = ""
-		if state == 'FIRST_HALF':
-			description += "Currently in first half.\n"
-		elif state == 'SECOND_HALF':
-			description += "Currently in second half.\n"
-
-		if starttime > now:
-			description += f"Starts at <t:{int(starttime.timestamp())}> (<t:{int(starttime.timestamp())}:R>)\n"
-		elif endtime > now:
-			description += f"Ends at <t:{int(endtime.timestamp())}> (<t:{int(endtime.timestamp())}:R>)\n"
-		else:
-			description += f"Ended at <t:{int(endtime.timestamp())}>\n"
-
-		embed = discord.Embed(colour=0x0004FF, description = description)
-		embed.title = splatfest['title']
-		embed.set_image(url = splatfest['image']['url'])
-
-		for t in splatfest['teams']:
-			id = base64.b64decode(t['id']).decode("utf-8")
-			which = re.sub('^.*:', '', id)
-			winner = False
-			if t.get('result'):
-				winner = t['result'].get('isWinner', False)
-			text = t['teamName']
-			if winner:
-				text += "\n**Winner!**"
-
-			embed.add_field(name = f'Team {which}', value = text)
-
-		return embed
-
-
-	@classmethod
-	def createSalmonRunResultsEmbed(cls, results):
-		embed = discord.Embed(colour=0x0004FF)
-		historyGroups = results['data']['coopResult']['historyGroups']['nodes']
-		stats = results['data']['coopResult']
-		pointCard = results['data']['coopResult']['pointCard']
-		embed.title = f"{lastGameDetails['afterGrade']['name']} - {stats['regularGradePoint']}"
-		embed.add_field(name = "Totals", value = f"Shifts Worked: {pointCard['playCount']}\nPower Eggs: {pointCard['deliverCount']}\nGolden Eggs: {pointCard['goldenDeliverCount']}\nTotal Points: {pointCard['totalPoint']}\nKing Salmonoid Kills: {pointCard['defeatBossCount']}", inline = True)
-		embed.add_field(name = "Scales", value = f"Bronze: {stats['scale']['bronze']}\nSilver: {stats['scale']['silver']}\nGold:{stats['scale']['gold']}", inline = True)
-		
-		bossSeen = 0
-		bossDowns = 0
-		clears = 0
-		pwrEggTotal = 0
-		gldEggTotal = 0
-		matches = 0
-		for group in historyGroups:
-			#Here for checking highest results
-			for match in group['historyDetails']['nodes']:
-				if match['bossResult'] != None:
-					bossSeen += 1
-					if match['bossResult']['hasDefeatBoss']:
-						bossDowns += 1
-
-				pwrEggTotal += match['myResult']['deliverCount']
-				gldEggTotal += match['myResult']['goldenDeliverCount']
-				if match['gradePointDiff'] == 'UP':
-					clears += 1
-				matches += 1
-
-		#embed.add_field(name = f"Average Stats (Last {matches})", value = "STUFF", inline = True)
-		embed.add_field(name = f"Total Stats (Last {matches})", value = f"King Salmonoids Seen: {bossSeen}\nKing Salmonids Clears: {bossDowns}\nGolden Eggs: {gldEggTotal}\nPower Eggs: {pwrEggTotal}", inline = True)
-
-		return embed
-
-	@classmethod
-	def createGearSubsEmbed(cls, embed, gear):
-		out = []
-		out.append(f"Main: {gear['primaryGearPower']['name']}")
-		for sub in gear['additionalGearPowers']:
-			out.append(f"Sub: {sub['name']}")
-
-		embed.add_field(name=f"{gear['__isGear'].replace('Gear', '')} - {gear['name']}", value="\n".join(out))
-
-	@classmethod
-	def createMultiplayerStatsEmbed(cls, statssimple, statsfull, species):
-		embed = discord.Embed(colour=0x0004FF)
-		name = statsfull['data']['currentPlayer']['name']
-		id = statsfull['data']['currentPlayer']['nameId']
-		shoes = statsfull['data']['currentPlayer']['shoesGear']
-		clothes = statsfull['data']['currentPlayer']['clothingGear']
-		head = statsfull['data']['currentPlayer']['headGear']
-		weapon = statsfull['data']['currentPlayer']['weapon']
-		byname = statsfull['data']['currentPlayer']['byname']
-		usedWeaps = statsfull['data']['playHistory']['frequentlyUsedWeapons']
-		paintpt = statsfull['data']['playHistory']['paintPointTotal']
-		maxRank = statsfull['data']['playHistory']['udemaeMax']
-		winCount = statsfull['data']['playHistory']['winCountTotal']
-		totalBattle = statssimple['data']['playHistory']['battleNumTotal']
-		species = species['data']['currentPlayer']['species']
-
-		embed.title = f"Multiplayer stats for {name}#{id} - {species.title()}"
-		percent = "{:.0f}".format(winCount/totalBattle * 100 if totalBattle > 0 else 0.0)
-		embed.add_field(name="Stats", value=f"Title: {byname}\nTotal Turf Inked: {str(paintpt)}\nBattles (Total/Won/Percent) : {str(totalBattle)}/{str(winCount)}/{str(percent)}%\n")
-		cls.createGearSubsEmbed(embed, head)
-		cls.createGearSubsEmbed(embed, clothes)
-		cls.createGearSubsEmbed(embed, shoes)
-
-		return embed
-
-	@classmethod
-	def createStoreEmbed(self, gear, brand, title, configData):
-		embed = discord.Embed(colour=0xF9FC5F)
-		imgHash = hashlib.sha224(f"{gear['id']}{gear['gear']['primaryGearPower']['name']}".encode()).hexdigest()
-		if not os.path.exists(f"{configData['web_dir']}/s3/gearcards/{imgHash}.png"):
-			S3ImageBuilder.createGearCard(gear['gear']).save(f"{configData['web_dir']}/s3/gearcards/{imgHash}.png")
-
-		embed.set_thumbnail(url=f"{configData['hosted_url']}/s3/gearcards/{imgHash}.png")
-
-		embed.title = title
-		embed.add_field(name = "Brand", value = gear['gear']['brand']['name'], inline = True)
-		embed.add_field(name = "Gear Name", value = gear['gear']['name'], inline = True)
-		embed.add_field(name = "Type", value = gear['gear']['__typename'].replace("Gear", ""), inline = True)
-		embed.add_field(name = "Main Ability", value = gear['gear']['primaryGearPower']['name'], inline = True)
-		embed.add_field(name = "Sub Slots", value = len(gear['gear']['additionalGearPowers']), inline = True)
-		embed.add_field(name = "Common Ability", value = brand.commonAbility().name(), inline = True)
-		embed.add_field(name = "Price", value = gear['price'], inline = True)
-
-		return embed
-
-	@classmethod
-	def createStoreListingEmbed(self, gearJson, fonts, configData):
-		embed = discord.Embed(colour=0xF9FC5F)
-		embed.title = "Splatoon 3 Splatnet Store Gear"
-		url = S3ImageBuilder.createStoreCanvas(gearJson, fonts, configData)
-		embed.set_image(url=url)
-		embed.set_footer("To order gear, run `/s3 order")
-		return embed
 
 class S3Handler():
 	def __init__(self, client, mysqlHandler, nsotoken, splat3info, configData, fonts, cachemanager):
@@ -284,7 +145,7 @@ class S3Handler():
 
 		species = nso.s3.get_species_cur_weapon()
 
-		embed = S3Utils.createMultiplayerStatsEmbed(statssimple, statsfull, species)
+		embed = S3EmbedBuilder.createMultiplayerStatsEmbed(statssimple, statsfull, species)
 		if self.webDir and self.hostedUrl:
 			imgUrl = S3ImageBuilder.createNamePlateImage(statsfull, self.fonts, self.configData)
 			embed.set_thumbnail(url=f"{imgUrl}?{str(time.time() % 1)}")
@@ -305,7 +166,7 @@ class S3Handler():
 			print(f"get_salmon_run_stats returned none for user {ctx.user.id}")
 			return
 
-		embed = S3Utils.createSalmonRunResultsEmbed(srstats)
+		embed = S3EmbedBuilder.createSalmonRunResultsEmbed(srstats)
 		await ctx.respond(embed = embed)
 
 	async def cmdFit(self, ctx):
@@ -342,7 +203,7 @@ class S3Handler():
 			print(f"get_splatfest_list returned none for user {ctx.user.id}")
 			return
 
-		embed = S3Utils.createSplatfestEmbed(festinfo['data']['festRecords']['nodes'][0])
+		embed = S3EmbedBuilder.createSplatfestEmbed(festinfo['data']['festRecords']['nodes'][0])
 		await ctx.respond(embed = embed)
 
 	async def cmdSchedule(self, ctx, which):
@@ -380,7 +241,7 @@ class S3Handler():
 
 	async def cmdStoreList(self, ctx):
 		if self.storedm.cacheState:
-			await ctx.respond(embed=S3Utils.createStoreListingEmbed(self.storedm.storecache, self.fonts, self.configData))
+			await ctx.respond(embed=S3EmbedBuilder.createStoreListingEmbed(self.storedm.storecache, self.fonts, self.configData))
 		else:
 			#TODO...
 			await ctx.respond("I can't fetch the current store listing, please try again later")
@@ -420,7 +281,7 @@ class S3Handler():
 				if ret['data']['orderGesotownGear']['userErrors'] == None:
 					#createStoreEmbed(self, gear, brand, title, configData):
 					brand = self.splat3info.brands.getItemByName(theItem['gear']['brand']['name'])
-					await ctx.respond(embed = S3Utils.createStoreEmbed(theItem, brand, "Ordered! Talk to Murch in game to get it!", self.configData))
+					await ctx.respond(embed = S3EmbedBuilder.createStoreEmbed(theItem, brand, "Ordered! Talk to Murch in game to get it!", self.configData))
 				elif ret['data']['orderGesotownGear']['userErrors'][0]['code'] == "GESOTOWN_ALREADY_ORDERED":
 					await ctx.respond("You already have an item on order! If you still want to order this, run this command again with Override set to True.")
 				else:
