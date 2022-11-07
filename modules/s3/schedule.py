d3c109d66b40f7928cc8a8968cbe06145e20302b
Andrew Wendt <andy@putergeek.com>
2022-11-07 13:57:37 -0800

    Parse Salmon Run schedule.

--- modules/s3/schedule.py
+++ modules/s3/schedule.py
@@ -3,6 +3,8 @@ import asyncio
 import time
 import requests
 import dateutil.parser
+import hashlib
+import base64
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -34,10 +36,15 @@ class S3Schedule():
 		self.updatetime = None
 		self.image_cache_small = cachemanager.open("s3.maps.small", (3600 * 24 * 90))  # Cache for 90 days
 
+		self.image_cache_sr_maps = cachemanager.open("s3.sr.maps", (3600 * 24 * 90))  # Cache for 90 days
+		self.image_cache_sr_weapons = cachemanager.open("s3.sr.weapons", (3600 * 24 * 90))  # Cache for 90 days
+
 		self.turf_war_schedule       = []
 		self.splatfest_schedule      = []
 		self.anarchy_open_schedule   = []
 		self.anarchy_series_schedule = []
+
+		self.salmon_run_schedule     = []
 
 		# Schedule updates every hour
 		self.scheduler = AsyncIOScheduler()
@@ -93,9 +100,26 @@ class S3Schedule():
 			map = {}
 			map['image']   = vs['image']['url']
 			map['name']    = vs['name']
-			map['stageid'] = vs['vsStageId']
+			map['stageid'] = vs['vsStageId']  # TODO: Use base64 of 'id' field instead?
 			maps.append(map)
 		return maps
+
+	# Given 'coopStage' object, returns the map
+	def parse_salmon_map(self, data):
+		map = {}
+		map['image']   = data['image']['url']
+		map['stageid'] = base64.b64decode(data['id']).decode("utf-8")
+		map['name']    = data['name']
+		return map
+
+	def parse_salmon_weapons(self, weapon_data):
+		weapons = []
+		for w in weapon_data:
+			weapon = {}
+			weapon['name'] = w['name']
+			weapon['image']  = w['image']['url']
+			weapons.append(weapon)
+		return weapons
 
 	def parse_schedule_turf(self, settings, rec):
 		rec['mode'] = settings['vsRule']['name']
@@ -134,6 +158,26 @@ class S3Schedule():
 
 		return recs
 
+	def parse_salmon_schedule(self, data):
+		if not data or not data.get('nodes'):
+			return []  # Empty
+
+		nodes = data.get('nodes')
+
+		recs = []
+		for node in nodes:
+			if node.get('setting') is None:
+				continue  # Nothing scheduled in this timeslot
+
+			rec = {}
+			rec['starttime'] = dateutil.parser.isoparse(node['startTime']).timestamp()
+			rec['endtime']   = dateutil.parser.isoparse(node['endTime']).timestamp()
+			rec['maps']      = [ self.parse_salmon_map(node['setting']['coopStage']) ]
+			rec['weapons']   = self.parse_salmon_weapons(node['setting']['weapons'])
+			recs.append(rec)
+
+		return recs
+
 	async def update(self):
 		if not self.is_update_needed():
 			return
@@ -158,11 +202,14 @@ class S3Schedule():
 		self.anarchy_open_schedule   = self.parse_schedule(data['data'].get('bankaraSchedules'), 'bankaraMatchSettings', self.parse_schedule_anarchy_open)
 		self.anarchy_series_schedule = self.parse_schedule(data['data'].get('bankaraSchedules'), 'bankaraMatchSettings', self.parse_schedule_anarchy_series)
 
-		self.cache_map_images()
+		self.salmon_run_schedule = self.parse_salmon_schedule(data['data'].get('coopGroupingSchedule', {}).get('regularSchedules'))
+
+		self.cache_images()
 
 		self.updatetime = time.time()
 
-	def cache_map_images(self):
+	def cache_images(self):
+		# PvP
 		for rec in [*self.turf_war_schedule, *self.splatfest_schedule, *self.anarchy_open_schedule, *self.anarchy_series_schedule]:
 			for map in rec['maps']:
 				if (not map['stageid']) or (not map['image']):
@@ -176,3 +223,29 @@ class S3Schedule():
 				response = requests.get(map['image'], stream=True)
 				self.image_cache_small.add_http_response(key, response)
 
+
+		# Salmon run
+		for rec in self.salmon_run_schedule:
+			for map in rec['maps']:
+				if (not map['stageid']) or (not map['image']):
+					continue  # Missing required fields
+
+				key = f"{map['stageid']}.png"
+				if self.image_cache_sr_maps.has(key):
+					continue  # Already cached
+
+				print(f"Caching SR map image stageid {map['stageid']} name '{map['name']}' image-url {map['image']}")
+				response = requests.get(map['image'], stream=True)
+				self.image_cache_salmon_maps.add_http_response(key, response)
+
+			for weapon in rec['weapons']:
+				if (not weapon['name']) or (not weapon['image']):
+					continue  # Missing required fields
+
+				key = f"weapon-{hashlib.sha1(weapon['name'].encode('utf-8')).hexdigest()}.png"
+				if self.image_cache_sr_weapons.has(key):
+					continue  # Already cached
+
+				print(f"Caching SR weapon name '{weapon['name']}' key '{key}' image-url {weapon['image']}")
+				response = requests.get(weapon['image'], stream=True)
+				self.image_cache_sr_weapons.add_http_response(key, response)
