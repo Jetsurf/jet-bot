40c6eb32cee2091414ad493f0c6b5c7ba9c444ad
Jetsurf <crmeade@mtu.edu>
2023-01-18 01:45:14 +0000

    Fixes for s3feeds

--- modules/s3/feedhandler.py
+++ modules/s3/feedhandler.py
@@ -19,8 +19,8 @@ class S3FeedHandler():
 		self.fonts = fonts
 		self.initialized = False
 		self.scheduler = AsyncIOScheduler(timezone='UTC')
-		self.scheduler.add_job(self.doMapFeed, 'cron', hour="*/2", minute='0', second='25', timezone='UTC')
-		self.scheduler.add_job(self.doGearFeed, 'cron', hour="*/4", minute='0', second='25', timezone='UTC')
+		self.scheduler.add_job(self.doMapFeed, 'cron', second='0', timezone='UTC')#hour="*/2", minute='0', second='25', timezone='UTC')
+		self.scheduler.add_job(self.doGearFeed, 'cron', second='0', timezone='UTC')#hour="*/4", minute='0', second='25', timezone='UTC')
 		asyncio.create_task(self.scheduleSRFeed())
 
 	async def scheduleSRFeed(self):
@@ -66,19 +66,26 @@ class S3FeedHandler():
 		image_io = S3ImageBuilder.createScheduleImage(timewindows, schedules, self.fonts, self.cachemanager, self.splat3info)
 		embed = discord.Embed(colour=0x0004FF)
 		embed.title = "Current Splatoon 3 multiplayer map rotation"
-		img = discord.File(image_io, filename = "maps-feed.png", description = "Current S3 multiplayer schedule")
-		embed.set_image(url = "attachment://maps-feed.png")
 
 		cur = await self.sqlBroker.connect()
 		await cur.execute("SELECT * from s3feeds WHERE (maps = 1)")
 		map_feeds = await cur.fetchall()
-		await self.sqlBroker.close(cur)
-
+		
 		print(f"Doing {len(map_feeds)} S3 map feeds")
 
 		for id in range(len(map_feeds)):
+			img = discord.File(image_io, filename = "maps-feed.png", description = "Current S3 multiplayer schedule")
+			embed.set_image(url = "attachment://maps-feed.png")
+			image_io.seek(0)
+
 			channel = self.client.get_guild(int(map_feeds[id][0])).get_channel(int(map_feeds[id][1]))
-			await channel.send(file = img, embed = embed)
+			if channel != None:
+				await channel.send(file = img, embed = embed)
+			else:
+				print(f"Deleting feed for channel {map_feeds[id][1]}")
+				await cur.execute("DELETE FROM s3feeds WHERE channelid = %s", (map_feeds[id][1],))
+
+		await self.sqlBroker.commit(cur)
 
 	async def doSRFeed(self):
 		await self.scheduleSRFeed() # Setup next run
@@ -87,19 +94,27 @@ class S3FeedHandler():
 		image_io = S3ImageBuilder.createSRScheduleImage(sched, self.fonts, self.cachemanager)
 		embed = discord.Embed(colour=0x0004FF)
 		embed.title = "Current Splatoon 3 Salmon Run rotation"
-		img = discord.File(image_io, filename = "sr-feed.png", description = "Current S3 Salmon Run schedule")
-		embed.set_image(url = "attachment://sr-feed.png")
+
 
 		cur = await self.sqlBroker.connect()
 		await cur.execute("SELECT * from s3feeds WHERE sr = 1")
 		srFeeds = await cur.fetchall()
-		await self.sqlBroker.close(cur)
 
 		print(f"Doing {len(srFeeds)} S3 Salmon Run feeds")
 
 		for id in range(len(srFeeds)):
+			img = discord.File(image_io, filename = "sr-feed.png", description = "Current S3 Salmon Run schedule")
+			embed.set_image(url = "attachment://sr-feed.png")
+			image_io.seek(0)
+
 			channel = self.client.get_guild(int(srFeeds[id][0])).get_channel(int(srFeeds[id][1]))
-			await channel.send(file = img, embed = embed)
+			if channel != None:
+				await channel.send(file = img, embed = embed)
+			else:
+				print(f"Deleting feed for channel {srFeeds[id][1]}")
+				await cur.execute("DELETE FROM s3feeds WHERE channelid = %s", (srFeeds[id][1],))
+
+		await self.sqlBroker.commit(cur)
 
 	async def doGearFeed(self):
 		embed = discord.Embed(colour=0x0004FF)
@@ -119,19 +134,25 @@ class S3FeedHandler():
 
 		embed = discord.Embed(colour=0x0004FF)
 		embed.title = "New gear in Splatoon 3 Splatnet store"
-		img = discord.File(image_io, filename = "gear-feed.png", description = "New gear posted to Splatoon 3 Splatnet")
-		embed.set_image(url = "attachment://gear-feed.png")		
-
+		
 		cur = await self.sqlBroker.connect()
 		await cur.execute("SELECT * FROM s3feeds WHERE gear = 1")
 		gearFeeds = await cur.fetchall()
-		await self.sqlBroker.close(cur)
 
 		print(f"Doing {len(gearFeeds)} S3 gear feeds")
 
 		for id in range(len(gearFeeds)):
+			img = discord.File(image_io, filename = "gear-feed.png", description = "New gear posted to Splatoon 3 Splatnet")
+			embed.set_image(url = "attachment://gear-feed.png")			
+			image_io.seek(0)
+
 			channel = self.client.get_guild(int(gearFeeds[id][0])).get_channel(int(gearFeeds[id][1]))
-			await channel.send(file = img, embed = embed)
-			
+			if channel != None:
+				await channel.send(file = img, embed = embed)
+			else:
+				print(f"Deleting feed for channel {gearFeeds[id][1]}")
+				await cur.execute("DELETE FROM s3feeds WHERE channelid = %s", (gearFeeds[id][1],))
+
+		await self.sqlBroker.commit(cur)
 		return
 
