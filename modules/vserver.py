cae7635052e8751f3b3b579246475f19cf152716
Jetsurf <crmeade@mtu.edu>
2024-06-10 22:57:03 +0000

    Last print

--- modules/vserver.py
+++ modules/vserver.py
@@ -295,7 +295,6 @@ class YTDLSource(discord.PCMVolumeTransformer):
 		loop = loop or asyncio.get_event_loop()
 		data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
-		print(f"{data}")
 		if data['extractor'] == 'generic' or data['extractor'] == 'youtube:search':
 			return None
 
